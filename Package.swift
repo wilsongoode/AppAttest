19b79813943fef9ccaa882bb2cd3eade6a563070
Wilson Goode <45737196+wilsongoode@users.noreply.github.com>
2026-01-21 03:49:07 -0500

    Bump minimum macOS

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 
 let package = Package(
     name: "AppAttest",
-    platforms: [.iOS(.v14), .macOS(.v10_15)],
+    platforms: [.iOS(.v14), .macOS(.v11)],
     products: [
         .library(
             name: "AppAttest",
